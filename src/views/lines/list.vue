bb15bf2bae1763da84f92de73ffce59f8a4b348e
shenyibo <1335638583@qq.com>
2020-06-28 16:23:00 +0800

    额度管理

--- src/views/lines/list.vue
+++ src/views/lines/list.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="container">
+    <el-row class="top">
+      <el-button>额度调整</el-button>
+      <el-button type="primary">额度转换</el-button>
+      <el-button type="success">任务退次</el-button>
+      <el-button type="info">额度延期</el-button>
+    </el-row>
     <el-table
       :data="tableData"
       border
@@ -7,17 +13,35 @@
     >
       <el-table-column
         prop="date"
+        align="center"
         label="日期"
         width="180"
       />
       <el-table-column
         prop="name"
-        label="姓名"
+        align="center"
+        label="客户名称"
         width="180"
       />
       <el-table-column
         prop="address"
-        label="地址"
+        align="center"
+        label="服务名称"
+      />
+      <el-table-column
+        prop="change"
+        align="center"
+        label="额度变化"
+      />
+      <el-table-column
+        prop="info"
+        align="center"
+        label="变化操作说明"
+      />
+      <el-table-column
+        prop="note"
+        align="center"
+        label="备注说明"
       />
     </el-table>
   </div>
@@ -31,20 +55,53 @@ export default {
     return {
       tableData: [{
         date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
+        name: '超级厉害娱乐信息公司',
+        address: '深度兼容--Android 300',
+        change: '-2',
+        info: '实施消耗，实施任务编号：12345678',
+        note: ''
       }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
+        date: '2016-05-02',
+        name: '超级厉害娱乐信息公司',
+        address: '深度兼容--Android 300',
+        change: '-2',
+        info: '实施消耗，实施任务编号：12345678',
+        note: ''
       }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
+        date: '2016-05-02',
+        name: '超级厉害娱乐信息公司',
+        address: '深度兼容--Android 300',
+        change: '-2',
+        info: '实施消耗，实施任务编号：12345678',
+        note: ''
       }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
+        date: '2016-05-02',
+        name: '超级厉害娱乐信息公司',
+        address: '深度兼容--Android 300',
+        change: '-2',
+        info: '实施消耗，实施任务编号：12345678',
+        note: ''
+      }, {
+        date: '2016-05-02',
+        name: '超级厉害娱乐信息公司',
+        address: '深度兼容--Android 300',
+        change: '-2',
+        info: '实施消耗，实施任务编号：12345678',
+        note: ''
+      }, {
+        date: '2016-05-02',
+        name: '超级厉害娱乐信息公司',
+        address: '深度兼容--Android 300',
+        change: '-2',
+        info: '实施消耗，实施任务编号：12345678',
+        note: ''
+      }, {
+        date: '2016-05-02',
+        name: '超级厉害娱乐信息公司',
+        address: '深度兼容--Android 300',
+        change: '-2',
+        info: '实施消耗，实施任务编号：12345678',
+        note: ''
       }]
     }
   }
@@ -52,5 +109,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+  .top {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 10px;
+  }
+}
 </style>

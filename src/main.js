b356d9915f201f05d28d847cdd05472da3772fad
shenyibo <1335638583@qq.com>
2020-10-12 19:13:29 +0800

    验收环境

--- src/main.js
+++ src/main.js
@@ -25,10 +25,10 @@ import * as filters from './filters' // global filters
  * Currently MockJs will be used in the production environment,
  * please remove it before going online ! ! !
  */
-if (process.env.NODE_ENV === 'production') {
-  const { mockXHR } = require('../mock')
-  mockXHR()
-}
+// if (process.env.NODE_ENV === 'production') {
+//   const { mockXHR } = require('../mock')
+//   mockXHR()
+// }
 
 Vue.use(Element, {
   size: 'small'

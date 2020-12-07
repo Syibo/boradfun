84f58f71130d11560fc81af02954d3b5aa649eb7
沈bb <oncwnuNLYlqqxZGCZUwlTwTxeng8@git.weixin.qq.com>
2020-12-07 22:57:26 +0800

    table

--- src/views/reimbursement/index.vue
+++ src/views/reimbursement/index.vue
@@ -1,6 +1,67 @@
 <template>
-  <div class="container">
-    我的报销
+  <div class="container rimbu-container">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="全部" name="first" />
+      <el-tab-pane label="待审核" name="second" />
+      <el-tab-pane label="待支付" name="third" />
+      <el-tab-pane label="已支付" name="four" />
+    </el-tabs>
+    <el-row :gutter="20" class="three">
+      <el-col :span="8">
+        <div class="three-item">s</div>
+      </el-col>
+      <el-col :span="8">
+        <div class="three-item">s</div>
+      </el-col>
+      <el-col :span="8">
+        <div class="three-item">s</div>
+      </el-col>
+    </el-row>
+    <el-row class="table-top">
+      <div class="left">
+        <el-input v-model="planDate" placeholder="编号" />
+        <el-date-picker
+          v-model="planDate"
+          style="width: 200px;margin-left: 10px"
+          type="month"
+          placeholder="选择日期"
+          format="yyyy 年 MM 月"
+          value-format="yyyy-MM-dd"
+        />
+      </div>
+      <div class="right">
+        <el-button type="primary">申请报销</el-button>
+      </div>
+    </el-row>
+
+    <el-table :data="tableData" style="width: 100%" :header-cell-style="{background:'#F7F8FA'}">
+      <el-table-column align="center" label="申请人">
+        <template slot-scope="scope">
+          <span class="bule-hover"> {{ scope.row.e_name }} </span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="start_time" align="center" label="开始时间" />
+      <el-table-column prop="duration" align="center" label="加班时长" />
+      <el-table-column v-permission="[6, 7, 8, 9, 10]" prop="real_duration" align="center" label="实际加班时长" />
+      <el-table-column prop="req_time" align="center" label="申请时间" />
+      <el-table-column prop="name" align="center" label="流程信息">
+        <template>
+          <el-button slot="reference" type="text">查看详情</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="broadfun_block">
+      <el-pagination
+        :current-page="seachValue.pagenum"
+        :page-sizes="[10, 20, 50]"
+        :page-size="seachValue.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
   </div>
 </template>
 
@@ -10,18 +71,48 @@ export default {
   name: 'RimbursementIndex',
   directives: { permission },
   data() {
-    return {}
+    return {
+      activeName: 'first',
+      seachValue: {
+        pagenum: 1,
+        pagesize: 10,
+        name: '',
+        type: '',
+        status: '',
+        myreq: true,
+        mytodo: ''
+      },
+      total: 0,
+      planDate: '',
+      tableData: [
+        { e_name: '#67566' }
+      ]
+    }
+  },
+  methods: {
+    handleClick() {},
+    handleSizeChange(val) {
+      this.seachValue.pagenum = 1
+      this.seachValue.pagesize = val
+      this.init()
+    },
+    handleCurrentChange(val) {
+      this.seachValue.pagenum = val
+      this.init()
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.container {
+.rimbu-container {
   padding-bottom: 50px;
-  .top {
-    display: flex;
-    justify-content: flex-end;
-    margin-bottom: 10px;
-  }
+ .three {
+   margin-bottom: 10px;
+   .three-item {
+     min-height: 100px;
+     background: #d3dce6;
+   }
+ }
 }
 </style>

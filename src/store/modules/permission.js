f8e5d1214ca9c84e115ad4f0ffee8b730329deb3
shenyibo <1335638583@qq.com>
2021-01-25 15:33:25 +0800

    add hrbp

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,5 +1,6 @@
 import { asyncRoutes, constantRoutes } from '@/router'
-
+import { getToken } from '@/utils/auth'
+import { SUPER_ADMIN_EMAIL, HR_BP } from '@/utils/const'
 /**
  * Use meta.role to determine if the current user has permission
  * @param roles
@@ -7,7 +8,23 @@ import { asyncRoutes, constantRoutes } from '@/router'
  */
 function hasPermission(roles, route) {
   if (route.meta && route.meta.roles) {
-    return roles.some(role => route.meta.roles.includes(role))
+    if (route.meta.super) {
+    // console.log()
+    // 第一个if是判断当前用户是不是马总
+    // 因为当前并没有一个超级管理员的权限， 马总的userTpye也是10
+    // 所以目前根据meta里面的super判断当前登录账号的Email是不是 SUPER_ADMIN_EMAIL
+      let userInfo = getToken()
+      if (typeof (day) === 'object') {
+        console.log('isSuper', userInfo)
+      } else {
+        userInfo = JSON.parse(userInfo)
+      }
+      let status = false
+      status = userInfo.email === SUPER_ADMIN_EMAIL || userInfo.email === HR_BP
+      return status
+    } else {
+      return roles.some(role => route.meta.roles.includes(role))
+    }
   } else {
     return true
   }
--- src/utils/const.js
+++ src/utils/const.js
@@ -78,3 +78,8 @@ export const WORKFLOW = [
 ]
 
 export const DOWNURL = 'http://172.16.9.195:9000'
+
+export const SUPER_ADMIN_EMAIL = 'ralph.ma@broadfun.cn'
+
+export const HR_BP = 'kk.chen@broadfun.cn'
+
